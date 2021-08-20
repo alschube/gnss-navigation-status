d612e3ecd9bcf9bdbc14d747ba6eaa1789e60f03
alschube <alschube@stud.hs-heilbronn.de>
2021-08-20 10:31:02 +0200

    added documentation and removed some commented lines

--- backend/gnss_data.py
+++ backend/gnss_data.py
@@ -1,10 +1,15 @@
-# This script is a part of the backend for my bachelor thesis
+# This project was developed in the context of my bachelor thesis.
+# It serves as a backend for my app to provide centimeter accurate positioning.
+#
+# The required data is received by the receiver and then passed on to the
+# Raspberry Pi via the UART to be further processed in the backend and sent on to the frontend.
+#
+# It also serves as an interface for the frontend to communicate with the receiver to configure it.
+#
+# The Ublox Python package was used for the implementation.
+# This package was also extended by some additional functions, which where not provided by u-blox
 #------------------------------------------------------------------------
 # Written by Aline Schubert, 2021
-
-import json
-import datetime
-import serial
 
 class GnssData:
     """
