115d5d1fbcee79631f8ddfa8092ebaeb1948c86c
alschube <alschube@stud.hs-heilbronn.de>
2021-08-23 12:37:23 +0200

    made graphics dynamic to screen size

--- frontend/app/src/main/java/com/example/gnssnavigationstatus/ui/map/map_components/Map.kt
+++ frontend/app/src/main/java/com/example/gnssnavigationstatus/ui/map/map_components/Map.kt
@@ -8,6 +8,11 @@ import com.example.gnssnavigationstatus.R
 import com.example.gnssnavigationstatus.data.GnssDataHolder
 import kotlin.math.cos
 import kotlin.math.sin
+import android.os.Build
+import android.view.WindowManager
+import android.view.WindowMetrics
+import androidx.annotation.RequiresApi
+
 
 /**
  * This class is responsible for drawing the map
@@ -18,13 +23,18 @@ import kotlin.math.sin
  * @param height the height of the screen
  */
 @SuppressLint("ViewConstructor")
+@RequiresApi(Build.VERSION_CODES.R)
 class Map(context: Context, width: Int, height: Int) : View(context) {
 
+    private var wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    private var metrics:WindowMetrics = wm.currentWindowMetrics
+    private var viewRight = metrics.bounds.right
+
     /** create some variables used for calculation*/
-    private var scale: Int = 5
-    private var textSize: Float = 50f
-    private var textSizeDoubled: Float = textSize * 2
-    private var textSizeQuartered: Float = textSize * 2 / 8
+    private var scale: Int = viewRight / 250
+    private var textLabelY:Float = 16f
+    private var textLabelX:Float = 12.5f
+    private var scaledTextSize:Float = viewRight / 30f
     private val thinStroke: Float = 3f
     private val thickStroke: Float = 6f
     private var centerX: Float = width.toFloat() / 2
@@ -33,17 +43,19 @@ class Map(context: Context, width: Int, height: Int) : View(context) {
     private val thirtyDegrees:Float = 30f
     private val sixtyDegrees:Float = 60f
     private val ninetyDegrees:Float = 90f
+    private val hundredScaled:Float = 100f * scale
+    private val scaledTextDistance:Float = 110f * scale
 
     /** create some more colors*/
     private val orange: Int = Color.rgb(251, 140, 0)
-    private val dkgreen: Int = Color.rgb(0, 137, 123)
-    private val lgreen: Int = Color.rgb(109, 201, 69)
-    private val lblue: Int = Color.rgb(93, 194, 244)
+    private val lGreen: Int = Color.rgb(109, 201, 69)
+    private val lBlue: Int = Color.rgb(93, 194, 244)
 
     /** create a paint for each type of object */
     private var dotPaint = Paint()
     private var circlePaint = Paint()
     private var textPaint = Paint()
+    private var boldTextPaint = Paint()
     private var satellitePaint = Paint()
     private var satIdTextPaint = Paint()
 
@@ -61,15 +73,19 @@ class Map(context: Context, width: Int, height: Int) : View(context) {
         dotPaint = createPaint(Color.BLACK, thickStroke, Paint.Style.STROKE)
 
         //defines the style of circles
-        circlePaint = createPaint(Color.BLACK, thinStroke, Paint.Style.STROKE)
+        circlePaint = createPaint(Color.GRAY, thinStroke, Paint.Style.STROKE)
 
         //defines the style of texts
         textPaint = createPaint(Color.BLACK, thinStroke, Paint.Style.FILL)
-        textPaint.textSize = textSize
+        textPaint.textSize = scaledTextSize
+
+        //defines the style of bold texts
+        boldTextPaint = createPaint(Color.BLACK, thinStroke, Paint.Style.FILL_AND_STROKE)
+        boldTextPaint.textSize = scaledTextSize
 
         //defines the style of satelliteIdentifier
         satIdTextPaint = createPaint(Color.BLACK, 8f, Paint.Style.FILL)
-        satIdTextPaint.textSize = 40f
+        satIdTextPaint.textSize = scaledTextSize
 
         //defines the style of the satellites
         satellitePaint = createPaint(Color.TRANSPARENT, thinStroke, Paint.Style.FILL)
@@ -79,8 +95,9 @@ class Map(context: Context, width: Int, height: Int) : View(context) {
         canvas?.drawCircle(centerX, centerY, ninetyDegrees * scale, circlePaint)
         canvas?.drawCircle(centerX, centerY, sixtyDegrees * scale, circlePaint)
         canvas?.drawCircle(centerX, centerY, thirtyDegrees * scale, circlePaint)
-        canvas?.drawLine(centerX, textSizeDoubled, centerX, height - textSizeDoubled, circlePaint) //vertical line
-        canvas?.drawLine(textSizeDoubled, centerY, width - textSize * 2.5f, centerY, circlePaint) //horizontal line
+        canvas?.drawLine(centerX,
+            centerY + hundredScaled, centerX, centerY - hundredScaled, circlePaint) //vertical line
+        canvas?.drawLine(centerX + hundredScaled, centerY, centerX - hundredScaled, centerY, circlePaint) //horizontal line
 
         //labels the coordinate system
         canvas?.drawText("90°", centerX, centerY, textPaint)
@@ -88,10 +105,10 @@ class Map(context: Context, width: Int, height: Int) : View(context) {
         canvas?.drawText("30°", centerX, (centerY - sixtyDegrees * scale), textPaint)
         canvas?.drawText("0°", centerX, (centerY - ninetyDegrees * scale), textPaint)
 
-        canvas?.drawText("N", centerX - textSize * 3 / 8, textSize + textSize / 2, textPaint)
-        canvas?.drawText("S", centerX - textSizeQuartered, height - textSize, textPaint)
-        canvas?.drawText("O", width - textSizeDoubled, centerY + textSize * 1 / 3, textPaint)
-        canvas?.drawText("W", textSize, height / 2 + textSize * 1 / 3, textPaint)
+        canvas?.drawText("N", centerX - textLabelX, centerY - scaledTextDistance, boldTextPaint)
+        canvas?.drawText("S", centerX - textLabelX, centerY + scaledTextDistance, boldTextPaint)
+        canvas?.drawText("O", centerX + scaledTextDistance, centerY + textLabelY, boldTextPaint)
+        canvas?.drawText("W", centerX - scaledTextDistance, centerY + textLabelY, boldTextPaint)
 
         // draw the satellites
         // first check if data is given
@@ -104,8 +121,8 @@ class Map(context: Context, width: Int, height: Int) : View(context) {
 
                 // find correct satellite type and the according color
                 when (sat.type) {
-                    context.getString(R.string.GAL_text) -> satellitePaint.color = lblue
-                    context.getString(R.string.GLO_text) -> satellitePaint.color = lgreen
+                    context.getString(R.string.GAL_text) -> satellitePaint.color = lBlue
+                    context.getString(R.string.GLO_text) -> satellitePaint.color = lGreen
                     context.getString(R.string.BDS_text) -> satellitePaint.color = Color.RED
                     context.getString(R.string.GPS_text) -> satellitePaint.color = orange
                 }
@@ -118,6 +135,7 @@ class Map(context: Context, width: Int, height: Int) : View(context) {
                     5f * scale,
                     satellitePaint
                 )
+                //draw the satellite identifier
                 canvas?.drawText(sat.satelliteIdentifier.toString(), positionIdentityVector[0].toFloat() - 25, positionIdentityVector[1].toFloat()+10, satIdTextPaint)
             }
         }
